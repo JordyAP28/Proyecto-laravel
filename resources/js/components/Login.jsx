d63e8fe3c985af1979990e0e39b3531131e152fa
JordyAP28 <jordymarcell2003@gmail.com>
2025-12-02 16:29:15 -0500

    cambios

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -1,40 +1,53 @@
 import React, { useState } from "react";
-import "../../css/estilo_login.css"; // tu CSS original de Blade
+import axios from "axios";
+import "../../css/estilo_login.css";
 
 export default function Login() {
   const [mensaje, setMensaje] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const email = e.target.email.value.trim();
     const password = e.target.password.value.trim();
 
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      setMensaje("Por favor ingrese un correo válido.");
-    } else if (password.length < 6) {
-      setMensaje("La contraseña debe tener al menos 6 caracteres.");
-    } else if (email === "usuario@correo.com" && password === "123456") {
-      setMensaje(`Bienvenido, ${email}`);
-    } else {
-      setMensaje("Credenciales incorrectas.");
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/login", {
+        email,
+        password,
+      });
+
+      // Guardar token
+      localStorage.setItem("token", response.data.token);
+
+      setMensaje("Inicio de sesión exitoso");
+
+      // Redirigir al panel
+      window.location.href = "/estudiante";
+
+    } catch (error) {
+      if (error.response) {
+        setMensaje(error.response.data.message);
+      } else {
+        setMensaje("Error de conexión con el servidor");
+      }
     }
   };
 
   return (
     <div className="login-horizontal-container">
-      {/* Lado izquierdo */}
       <div className="login-left">
         <h2>Iniciar Sesión</h2>
 
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label>Correo electrónico</label>
-            <input type="email" name="email" placeholder="usuario@correo.com" required />
+            <input type="email" name="email" required />
           </div>
 
           <div className="input-group">
             <label>Contraseña</label>
-            <input type="password" name="password" placeholder="********" required />
+            <input type="password" name="password" required />
           </div>
 
           <button type="submit">Ingresar</button>
@@ -48,7 +61,6 @@ export default function Login() {
         </div>
       </div>
 
-      {/* Lado derecho */}
       <div className="login-right">
         <img
           src="https://www.teatrocentrodearte.org/images/files/2024/0f2f6cd5-31d9-44e4-9c83-e95bf046cb9d.webp"
